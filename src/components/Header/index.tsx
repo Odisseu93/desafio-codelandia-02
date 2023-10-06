990f7b58f5ab415a3929b77749d86142bf69730a
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-06 15:23:19 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+
+const Header = () => (
+	<header className="flex justify-center p-3 item-center bg-brand">
+		<p className="font-[500]">Ganhe R$ 10,00 de desconto no frete</p>
+	</header>
+)
+
+export default Header
