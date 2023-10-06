58796ea2d765ccb8c761efef46a663ff6c345791
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-06 10:54:05 -0300

    feat: add initial files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		colors: {
+			brand: '#00AEFF',
+	
+			/* dark scale */
+			dark: {
+				10: '#181818', /* 100% */
+				20: '#474747', /* 70% */
+				30: '#EBE9EA', /* 20% */
+				40: '#F9F9F9', /* 10% */
+			},
+	
+			/* color system */
+			system: {
+				message: '#2962FF',
+				success: '#0BB07B',
+				warning: '#FFCE52',
+				error: '#F03D3D',
+				info: '#E0E0E0',
+	
+			}
+		}
+	},
 }
 
